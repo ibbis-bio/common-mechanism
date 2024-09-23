4ea078d502803797f6d433b2063da6c83cf1c243
root <root@Michael-Laptop>
2024-09-23 14:04:35 +1200

    Additional database testing functionality, to check against pre-generated inputs, and also generating those inputs when necessary.
    updated database test to include some expected changes when comparing the exemplar outputs to those generated in other directory roots.
    
    Rename tools to just the specific tool they handle
    
    Rename database_handler -> search_handler
    
    Use abc module for search_handler
    
    functional renaming in screen_tools
    
    Improve database validation
    
    format w/ black
    
    Fixed bug with blast databases not validating correctly due to misnamed function override _validate vs validate. Updated functional DB test to use search() instead of Legacy screen()
    
    Minor format changes to search handlers, update to ensure bad file and directory validation is occuring for all database types during pytest.
    
    Tidying of screen.py, remove whitespace, alter line lengths for readibility.
    
    Updated blast handler derived ABC to also use abstractmethod for search().
    
    Merged with updated refactor

--- commec/tools/blastx.py
+++ commec/tools/blastx.py
@@ -10,7 +10,6 @@ import subprocess
 from commec.tools.blast_tools import BlastHandler
 from commec.tools.search_handler import SearchToolVersion
 
-
 class BlastXHandler(BlastHandler):
     """
     A search handler specifically for BLASTX command-line during commec screening.
