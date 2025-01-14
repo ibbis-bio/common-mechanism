5f51df13df04a8da97c923a749220ee462859d52
Michael Barnett <ToMichaelBarnett@gmail.com>
2025-01-14 14:03:53 +1300

    Added aa to nt unit tet, needs int64 fix.

--- commec/utils/coordinates.py
+++ commec/utils/coordinates.py
@@ -45,18 +45,11 @@ def convert_protein_to_nucleotide_coords(frame,
     nuc_start_reverse = (protein_start[reverse_mask] * 3) + (reverse_frame - 1)
     nuc_end_reverse = (protein_end[reverse_mask] * 3) + 2 + (reverse_frame - 1)
 
-    nucleotide_start[reverse_mask] = seq_length[reverse_mask] - nuc_end_reverse + 1
-    nucleotide_end[reverse_mask] = seq_length[reverse_mask] - nuc_start_reverse + 1
+    nucleotide_start[reverse_mask] = seq_length[reverse_mask] - nuc_end_reverse - 1
+    nucleotide_end[reverse_mask] = seq_length[reverse_mask] - nuc_start_reverse - 1
 
     # Convert to back to 1-based coordinates for reporting.
     nucleotide_start += 1
     nucleotide_end += 1
-
-    print(frame)
-    print(seq_length)
-    print(protein_start)
-    print(nucleotide_start)
-    print(protein_end)
-    print(nucleotide_end)
 
     return nucleotide_start, nucleotide_end
