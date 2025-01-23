001b6087dbd634beb8753ae7071fa453f80065ed
Tessa Alexanian <tessa@ibbis.bio>
2025-01-22 17:43:32 -0800

    move test_aa_to_nt into test_hmmer

--- commec/tests/test_hmmer.py
+++ commec/tests/test_hmmer.py
@@ -9,7 +9,12 @@ The following behaviour is expected:
 
 import pandas as pd
 import pytest
-from commec.tools.hmmer import remove_overlaps, get_frame_from_query_name
+from pandas.testing import assert_frame_equal
+from commec.tools.hmmer import (
+    remove_overlaps,
+    get_frame_from_query_name,
+    set_query_coordinates,
+)
 
 # Test the following hmmer configuration:
 # 10-----------------50 (Largest, should stay.)
@@ -20,30 +25,34 @@ from commec.tools.hmmer import remove_overlaps, get_frame_from_query_name
 # 10------------------------------90 (different query)
 
 # Example DataFrame
-example_hmmer_01 = pd.DataFrame({
-    "query name": ["one","one","one","one","two", "two"],
-    "q. start": [10, 40, 20, 10, 20, 10],
-    "q. end":   [50, 60, 40, 50, 30, 90],
-    "score":    [3, 5, 6, 1, 1, 2]
-})
+example_hmmer_01 = pd.DataFrame(
+    {
+        "query name": ["one", "one", "one", "one", "two", "two"],
+        "q. start": [10, 40, 20, 10, 20, 10],
+        "q. end": [50, 60, 40, 50, 30, 90],
+        "score": [3, 5, 6, 1, 1, 2],
+    }
+)
 
 # Example DataFrame
-example_hmmer_01_output = pd.DataFrame({
-    "query name": ["one","one", "one", "two"],
-    "q. start": [10, 40, 20, 10],
-    "q. end":   [50, 60, 40, 90],
-    "score":    [3, 5, 6, 2]
-})
+example_hmmer_01_output = pd.DataFrame(
+    {
+        "query name": ["one", "one", "one", "two"],
+        "q. start": [10, 40, 20, 10],
+        "q. end": [50, 60, 40, 90],
+        "score": [3, 5, 6, 2],
+    }
+)
+
 
 @pytest.mark.parametrize(
     "input_hmmer, expected_output_hmmer",
     [
         (example_hmmer_01, example_hmmer_01_output),
-    ]
+    ],
 )
 def test_remove_overlaps(
-    input_hmmer : pd.DataFrame,
-    expected_output_hmmer : pd.DataFrame
+    input_hmmer: pd.DataFrame, expected_output_hmmer: pd.DataFrame
 ):
     """
     Checks common configurations that require trimming in Hmmer outputs,
@@ -60,10 +69,57 @@ def test_remove_overlaps(
 
 
 def test_get_frame_from_query_name():
-    example_hmmer_01_output = pd.DataFrame({
-        "query name": ["one","one", "one", "two"],
-        "q. start": [10, 40, 20, 10],
-        "q. end":   [50, 60, 40, 90],
-        "score":    [3, 5, 6, 2]
-    })
+    example_hmmer_01_output = pd.DataFrame(
+        {
+            "query name": ["one", "one", "one", "two"],
+            "q. start": [10, 40, 20, 10],
+            "q. end": [50, 60, 40, 90],
+            "score": [3, 5, 6, 2],
+        }
+    )
     get_frame_from_query_name(example_hmmer_01_output["query name"])
+
+
+def test_set_query_coordinates():
+    input_hmmer = pd.DataFrame(
+        {
+            "query name": ["F1", "F2", "F3", "R1", "R2", "R3"],
+            "frame": [1, 2, 3, 4, 5, 6],
+            "ali from": [1, 2, 3, 1, 2, 3],
+            "ali to": [4, 5, 6, 4, 5, 6],
+            "qlen": [10, 9, 9, 10, 9, 9],
+        }
+    )
+
+    # 1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30
+    # [  1  ]  [  2  ]  [  3  ] [   4  ] [   5  ] [   6  ] [   7  ] [   8  ] [   9  ] [  10  ]
+    #    [  1  ]  [  2  ]  [  3  ] [   4  ] [   5  ] [   6  ] [   7  ] [   8  ] [   9  ]
+    #       [  1  ]  [  2  ]  [  3  ] [   4  ] [   5  ] [   6  ] [   7  ] [   8  ] [   9  ]
+    # [  10 ]  [  9  ]  [  8  ] [   7  ] [   6  ] [   5  ] [   4  ] [   3  ] [   2  ] [   1  ]
+    #       [  9  ]  [  8  ] [   7  ] [   6  ] [   5  ] [   4  ] [   3  ] [   2  ] [   1  ]
+    #    [  9  ]  [  8  ] [   7  ] [   6  ] [   5  ] [   4  ] [   3  ] [   2  ] [   1  ]
+
+    # Example DataFrame
+    hmmer_with_query_coords = pd.DataFrame(
+        {
+            "query name": ["F1", "F2", "F3", "R1", "R2", "R3"],
+            "frame": [1, 2, 3, 4, 5, 6],
+            "ali from": [1, 2, 3, 1, 2, 3],
+            "ali to": [4, 5, 6, 4, 5, 6],
+            "qlen": [10, 9, 9, 10, 9, 9],
+            "q. start": [1, 5, 9, 19, 15, 11],
+            "q. end": [12, 16, 20, 30, 26, 22],
+        }
+    )
+
+    print("INPUT:")
+    print(input_hmmer)
+    print(input_hmmer.dtypes)
+    set_query_coordinates(input_hmmer)
+    print("PROCESSED:")
+    print(input_hmmer)
+    print(input_hmmer.dtypes)
+    print("CORRECT:")
+    print(hmmer_with_query_coords)
+    print(hmmer_with_query_coords.dtypes)
+    assert_frame_equal(input_hmmer, hmmer_with_query_coords)
