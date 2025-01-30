4ff9e100313c6c67907f6d5bc45b8240ea66c15e
Michael Michael <MichaelBarnett@users.noreply.github.com>
2025-01-31 11:51:53 +1300

    Hmmer indexing fix (#26)
    * Added coordinates, which deals with the conversion logic between NT and AA, currently has some off by one errors which will need addressing, but is designed to work with vectorisation, so should be very fast for large datasets. Noticed that TRIM hmmer doesn't Trim the same / correctly if its found in reverse compliment.
    
    * Rewrote a new trim hmmer, which has no trimming bugs related to re-indexing issues, i j for loop iteration misses, and works on the query nucleotide coordinates, and thus will respect biorisks in other reading frames etc when overlapping.
    
    * removed debugging print statements.
    
    * Removed trimhmmer.
    
    * Added simple unit test for remove overlaps, as well as updated remove overlaps to remove high scored, but encapsulated hits.
    
    * Actually add the tests file.
    
    * Added aa to nt unit tet, needs int64 fix.
    
    * Fixed int64 issue for q. start and q. end column dtypes for calculating from AA to NT coords.
    
    * Updated trimming, such that high scoring hits, which are encapsulated by low scoring larger ranges, are not trimmed.
    
    * Updated smaller qlens for frames 2,3 5,6 affecting the true calculation for NT from AA  conversions.
    
    ---------
    
    Co-authored-by: Michael Barnett <ToMichaelBarnett@gmail.com>
    
    Tessa suggestions 2 (#32)
    
    * large set of proposed renames in result.py (plus more descriptions for status)
    
    * Updated ScreenStatus descriptions such that they don't break things.
    
    ---------
    
    Co-authored-by: Tessa Alexanian <tessa@ibbis.bio>
    Co-authored-by: Michael Barnett <ToMichaelBarnett@gmail.com>
    
    Updated hmmer fix to take into account the nucleotide query length, derived from query dict, rather than guess.

---
+++ commec/tests/test_trim.py
@@ -0,0 +1,59 @@
+"""
+Simple tests to ensure the correct trimming of overlapping components in a Hmmer database
+when parsed to remove_overlaps.
+The following behaviour is expected:
+ * Fully encapsulated hits, should be removed.
+ * Partially overlapping hits, should both be kept (To maximise extents)
+ * Hits from different queries are independant in logic.
+"""
+
+import pandas as pd
+import pytest
+from commec.tools.hmmer import remove_overlaps
+
+# Test the following hmmer configuration:
+# 10-----------------50 (Largest, should stay.)
+#               40-------60 (Extends 1. Should stay.)
+#      20-------40      (encapsulated, but high score, should stay!)
+# 10-----------------50 (lower score than 1, should be removed.)
+#      20---30 (Different query, removed)
+# 10------------------------------90 (different query)
+
+# Example DataFrame
+example_hmmer_01 = pd.DataFrame({
+    "query name": ["one","one","one","one","two", "two"],
+    "q. start": [10, 40, 20, 10, 20, 10],
+    "q. end":   [50, 60, 40, 50, 30, 90],
+    "score":    [3, 5, 6, 1, 1, 2]
+})
+
+# Example DataFrame
+example_hmmer_01_output = pd.DataFrame({
+    "query name": ["one","one", "one", "two"],
+    "q. start": [10, 40, 20, 10],
+    "q. end":   [50, 60, 40, 90],
+    "score":    [3, 5, 6, 2]
+})
+
+@pytest.mark.parametrize(
+    "input_hmmer, expected_output_hmmer",
+    [
+        (example_hmmer_01, example_hmmer_01_output),
+    ]
+)
+def test_hmmer_overlaps(
+    input_hmmer : pd.DataFrame,
+    expected_output_hmmer : pd.DataFrame
+):
+    """
+    Checks common configurations that require trimming in Hmmer outputs,
+    In particular partial overlaps, full encapsulations, score differences, and different queries.
+    """
+    trimmed_input = remove_overlaps(input_hmmer)
+    print("INPUT:")
+    print(input_hmmer)
+    print("TRIMMED:")
+    print(trimmed_input)
+    print("CORRECT:")
+    print(expected_output_hmmer)
+    assert trimmed_input.equals(expected_output_hmmer)
