07d44a0cba916abec938168b0a9fe3e37e51c2d7
Michael Barnett <ToMichaelBarnett@gmail.com>
2025-02-26 10:41:38 +1300

    Updated benign to not clear biorisks, updated functional test to reflect new funcitonality.

--- commec/config/io_parameters.py
+++ commec/config/io_parameters.py
@@ -41,7 +41,7 @@ class ScreenIO:
         # IO files
         self.output_screen_file = f"{self.directory_prefix}.log"
         self.output_json = f"{self.directory_prefix}.output.json"
-        self.tmp_log = f"{self.directory_prefix}.log.tmp"
+        self.tmp_log = f"{self.output_prefix}.log.debug"
         self.nt_path = f"{self.input_prefix}.cleaned.fasta"
         self.aa_path = f"{self.input_prefix}.faa"
         self.nc_path = f"{self.input_prefix}.noncoding.fasta"
