1a12e861af862f9ed51787a744c58467f4bc8134
Tessa Alexanian <alexanian@users.noreply.github.com>
2025-03-07 16:20:30 -0500

    Rewrite commec flag for robustness (#39)
    ## Background
    `commec flag` was not behaving as expected  in a few cases, which was related both to some logging issues (will open a separate PR for this) and the script itself not being written in a terribly robust way. I grabbed some of the logic I wrote for the summarize_screens script [back in August](https://github.com/ibbis-bio/common-mechanism/pull/12) and have improved commec flag.
    
    ## Changes
    ### Bug fixes
    * Flag will recognize FAST mode with current logging
    * check_biorisk will return 1 when exiting with an error
    * A few miscellaneous errors due to `commec flag` previously not parsing each step separately
    
    ### New features
    * `commec flag` creates 3 files, including the new `screen_pipeline_status` file, explained below
    
    ## Notes on new flags output
    
    `commec flag` now produces 3 CSVs. Each line in each CSV corresponds to a .screen file.
    
    The **flags.csv** file has column "filename" and the following flag status columns: "biorisk", "virulence_factor", "regulated_virus", "regulated_bacteria", "regulated_eukaryote", "mixed_regulated_and_non_reg". Those columns can take the following values:
    ```
      * F 	flagged
      * P 	no flags
      * - 	not run
      * Err   error logged in this step
    ```
    while the final "benign" column can take the following values:
    ```
      * F 	flags not cleared
      * P 	all flags cleared
    ```
    The `flags_recommended` CSV just has two columns, without a header, containing the filename and the overall "F" or "P" for the file.
    
    The new **screen_pipeline_status.csv** file has a "name" and "filepath" column, as well as columns for each step of screening, "biorisk", "protein", "nucleotide", and "benign". These columns can take the following values:
    ```
      * flag      	the sequence was flagged in this step
      * pass      	the sequence passed in this step
      * skip      	this step was intentionally not run
      * error     	an error occurred during this step
      * -         	this step was not run due to an error, interrupt, or other unexpected outcome
      * mix       	(protein / nucloetide only) equally-good best match is to regulated- and
                  	non-regulated organisms
      * warn      	(biorisk only) significant hit to a virulence factor, but these are often shared
                  	between regulated and non-regulated organisms
      * cleared   	(benign only) earlier flags were cleared
      * not cleared   (benign only) earlier flags were not cleared
    ```
    Note that the biorisk, protein and nucleotide search columns may take multiple values due to having multiple hits.
    
    The screen_pipeline status also includes six boolean columns. Three indicate whether the sequence was flagged as a regulated virus, bacteria, or eukaryote, and three indicate whether the benign search cleared any regulated regions based on matches to benign protein, RNA, or DNA.

---
+++ commec/tests/test_flag.py
@@ -0,0 +1,68 @@
+import pytest
+import os
+import argparse
+import textwrap
+
+from commec.flag import add_args, run
+
+SCREEN_DIR = os.path.join(os.path.dirname(__file__), "test_data/screen-files")
+
+def test_flag(tmp_path):
+    """We are lazily writing tests for a full run of flag instead of unit tests."""
+    parser = argparse.ArgumentParser()
+    add_args(parser)
+    args = parser.parse_args([SCREEN_DIR, "-o", str(tmp_path)])
+    run(args)
+
+    # Check if the output file exists
+    status_output = tmp_path / "screen_pipeline_status.csv"
+    assert status_output.exists()
+
+    expected_status = textwrap.dedent(
+        f"""\
+        name,filepath,flag,biorisk,protein,nucleotide,benign,virus_flag,bacteria_flag,eukaryote_flag,benign_protein,benign_rna,benign_dna
+        biorisk-error-2025-02,{SCREEN_DIR}/biorisk-error-2025-02.screen,error,error,-,-,-,,,,,,
+        fast-mode-2025-02,{SCREEN_DIR}/fast-mode-2025-02.screen,pass,warn,skip,skip,skip,,,,,,
+        no-hits-2024-06,{SCREEN_DIR}/no-hits-2024-06.screen,pass,pass,-,-,skip,,,,,,
+        prot-error-2024-08,{SCREEN_DIR}/prot-error-2024-08.screen,error,pass,error,-,-,,,,,,
+        prot-hit-not-cleared-2024-06,{SCREEN_DIR}/prot-hit-not-cleared-2024-06.screen,flag,pass,flag,skip,not-cleared,True,False,False,False,False,False
+        prot-mixed-hit-2024-06,{SCREEN_DIR}/prot-mixed-hit-2024-06.screen,pass,pass,mix,skip,skip,False,False,False,,,
+        prot-multiple-hits-2024-06,{SCREEN_DIR}/prot-multiple-hits-2024-06.screen,flag,warn,flag;mix,pass,not-cleared,False,True,False,True,False,False
+        prot-nt-hits-cleared-2024-09,{SCREEN_DIR}/prot-nt-hits-cleared-2024-09.screen,pass,pass,flag,flag,cleared,False,True,False,False,True,True
+        """
+    )
+    actual_status = status_output.read_text()
+    assert expected_status.strip() == actual_status.strip()
+
+    flags_output = tmp_path / "flags.csv"
+    assert flags_output.exists()
+    expected_flags = textwrap.dedent(
+        f"""\
+        filename,biorisk,virulence_factor,regulated_virus,regulated_bacteria,regulated_eukaryote,mixed_regulated_and_non_reg,benign
+        {SCREEN_DIR}/biorisk-error-2025-02.screen,Err,Err,-,-,-,-,-
+        {SCREEN_DIR}/fast-mode-2025-02.screen,P,F,-,-,-,-,-
+        {SCREEN_DIR}/no-hits-2024-06.screen,P,P,-,-,-,-,-
+        {SCREEN_DIR}/prot-error-2024-08.screen,P,P,Err,Err,Err,Err,-
+        {SCREEN_DIR}/prot-hit-not-cleared-2024-06.screen,P,P,F,P,P,P,F
+        {SCREEN_DIR}/prot-mixed-hit-2024-06.screen,P,P,P,P,P,F,-
+        {SCREEN_DIR}/prot-multiple-hits-2024-06.screen,P,F,P,F,P,F,F
+        {SCREEN_DIR}/prot-nt-hits-cleared-2024-09.screen,P,P,P,F,P,P,P
+        """
+    )
+    assert expected_flags.strip() == flags_output.read_text().strip()
+
+    recomendations_output = tmp_path / "flags_recommended.csv"
+    assert recomendations_output.exists()
+    expected_recomendations = textwrap.dedent(
+        f"""\
+        {SCREEN_DIR}/biorisk-error-2025-02.screen,Err
+        {SCREEN_DIR}/fast-mode-2025-02.screen,P
+        {SCREEN_DIR}/no-hits-2024-06.screen,P
+        {SCREEN_DIR}/prot-error-2024-08.screen,Err
+        {SCREEN_DIR}/prot-hit-not-cleared-2024-06.screen,F
+        {SCREEN_DIR}/prot-mixed-hit-2024-06.screen,P
+        {SCREEN_DIR}/prot-multiple-hits-2024-06.screen,F
+        {SCREEN_DIR}/prot-nt-hits-cleared-2024-09.screen,P
+        """
+    )
+    assert expected_recomendations.strip() == recomendations_output.read_text().strip()
