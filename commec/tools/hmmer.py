9b617af52b86ccc92eaab6cd1bdd2d0c17032cfa
Michael Barnett <ToMichaelBarnett@gmail.com>
2025-03-23 12:57:59 +1300

    Updated biorisk for more verbose debug logging. Updated hitresult for str method for easy print. Removed legacy check_biorisk.

--- commec/tools/hmmer.py
+++ commec/tools/hmmer.py
@@ -186,5 +186,8 @@ def recalculate_hmmer_query_coordinates(hmmer : pd.DataFrame):
         hmmer["nt_qlen"].to_numpy())
 
 def append_nt_querylength_info(hmmer : pd.DataFrame, queries : dict[str, Query]):
-    """ Take the hmmer output, and add a series of the true nt length based on query name."""
+    """ 
+    Take the hmmer output, and add a series (nt_qlen) 
+    of the true nt length based on query name.
+    """
     hmmer["nt_qlen"] = [len(queries[q[:-2]].seq_record.seq) for q in hmmer["query name"]]
