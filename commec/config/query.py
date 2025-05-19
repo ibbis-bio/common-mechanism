f02580794fdca02b8857217a1c23de12f2c80903
Michael Barnett <ToMichaelBarnett@gmail.com>
2025-05-19 17:47:53 +1200

    Transferred specifically emboss dependancy removal. Updated test to include forms of ambiguous codes.

--- commec/config/query.py
+++ commec/config/query.py
@@ -2,6 +2,8 @@
 # Copyright (c) 2021-2025 International Biosecurity and Biosafety Initiative for Science
 
 import subprocess
+import os
+from Bio import Seq
 from Bio.SeqRecord import SeqRecord
 from dataclasses import dataclass
 from commec.config.result import QueryResult
@@ -35,17 +37,71 @@ class Query:
     def sequence(self) -> str:
         return str(self._seq_record.seq)
 
-    def translate(self, input_path, output_path) -> None:
-        """Run command transeq, to translate our input sequences."""
+    def translate(self, output_path: str | os.PathLike) -> None:
+        """
+        Append the six-frame translation of the query to the output file.
+        """
+        self._translate()
+        with open(output_path, "a", encoding="utf-8") as outfile:
+            for translation in self.translations:
+                outfile.write(f">{self.name}_{translation.frame}\n")
+                outfile.write(f"{translation.sequence}\n")
 
-        # TODO: Update line 53-55 of Check_Benign, to ensure that the query filter is using
-        # The correct name, when filtering benign components.
-        command = ["transeq", input_path, output_path, "-frame", "6", "-clean"]
-        result = subprocess.run(command, check = False, capture_output=True)
-        if result.returncode != 0:
-            raise RuntimeError(
-                f"Input FASTA {input_path} could not be translated:\n{result.stderr}"
+    def _translate(self) -> None:
+        """
+        Get the six-frame translations of the query sequence in all 6 reading frames.
+
+        Frame numbers follow the same naming convention as transeq:
+            * 1, 2, 3: Forward frames starting at positions 0, 1, 2
+            * 4, 5, 6: Reverse frames, starting at positions 0, -1, -2
+
+        Offsets are a little complicated. Taking the 11-nt sequence 'atgtgccatgg' as an example:
+
+            Frame   Pos     Codon split         Translation
+            1       0       atg tgc cat gg      MCH
+            2       1       a tgt gcc atg g     CAM
+            3       2       at gtg cca tgg      VPW
+            4       -0      cc atg gca cat      MAH
+            5       -1      c cat ggc aca t     HGT
+            6       -2      cca tgg cac at      PWH
+
+        As in previous `transeq -clean` command, all stop codons (*) are replaced with (X).
+
+        One *difference* from transeq is that we only translate full codons. So the frame 1 in the
+        example above is translated as MCH, rather than MCHG, even though gg will translate to
+        glycine (G) no matter what the subsequent nucleotide is.
+        """
+        self.translations = []
+        seq_rev = Seq.reverse_complement(self.sequence)
+
+        # Frames use offset 0, 1, 2
+        for offset in range(3):
+            frame_len = self._get_frame_length(offset)
+
+            # Forward frame is offset from the start of the sequence
+            f_start = offset
+            f_end = offset + frame_len
+            protein = str(Seq.translate(self.sequence[f_start:f_end], stop_symbol="X"))
+            self.translations.append(
+                QueryTranslation(sequence=protein, frame=offset + 1)
             )
+            # Reverse frame is offset from the end of the sequence
+            r_start = self.length - offset - frame_len
+            r_end = self.length - offset
+            protein = str(Seq.translate(seq_rev[r_start:r_end], stop_symbol="X"))
+            self.translations.append(
+                QueryTranslation(sequence=protein, frame=offset + 4)
+            )
+
+        # Sort the list in frame order
+        self.translations = sorted(self.translations, key=lambda x: x.frame)
+
+    def _get_frame_length(self, frame_offset: int) -> int:
+        """
+        Get total length of nt sequence that will be translated based on frame offset.
+        """
+        # Use integer division to get frame length based on starting offset
+        return 3 * ((self.length - frame_offset) // 3)
 
     @staticmethod
     def validate_sequence_record(seq_record: SeqRecord) -> None:
