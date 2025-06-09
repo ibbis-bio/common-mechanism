30a41f6ce4f3d2c94bd06cbb95f0c49f751a1e96
Michael Michael <MichaelBarnett@users.noreply.github.com>
2025-06-10 09:07:04 +1200

    Update protein rationale text
    Co-authored-by: Tessa Alexanian <alexanian@users.noreply.github.com>

--- commec/config/result.py
+++ commec/config/result.py
@@ -284,7 +284,7 @@ class QueryScreenStatus:
 
         if self.protein_taxonomy_status == ScreenStatus.FLAG:
             self.screen_status = ScreenStatus.FLAG
-            self.rationale = "A regulated protein sequence was detected"
+            self.rationale = "Matched a protein sequence from a regulated organism"
             return
 
         if self.nucleotide_taxonomy_status == ScreenStatus.FLAG:
