0198b03a7d7aad73c1c790564b1f12138adf02fa
Michael Barnett <ToMichaelBarnett@gmail.com>
2025-06-27 15:44:58 +1200

    Updated deep update for json usage to also present a list of keys which are not present and unexpected, gives warnings to the user for any bad yaml inputs that are unexpected.

--- commec/utils/dict_utils.py
+++ commec/utils/dict_utils.py
@@ -3,17 +3,25 @@
 """
 Static functions useful for dealing with common dictionary tasks.
 """
+import logging
+logger = logging.getLogger(__name__)
 
 @staticmethod
 def deep_update(to_update: dict, has_updates: dict):
     """
     Recursively update a nested dictionary without completely overwriting nested dictionaries.
     """
+    rejected = []
     updated = to_update.copy()
     for key, value in has_updates.items():
         # If both values are dictionaries, recursively update
         if key in updated and isinstance(updated[key], dict) and isinstance(value, dict):
-            updated[key] = deep_update(updated[key], value)
-        else:
+            updated[key], additional_rejects = deep_update(updated[key], value)
+            rejected.extend(additional_rejects)
+        # If not a dictionary, just copy the value.
+        elif key in updated:
             updated[key] = value
-    return updated
+        # If not present, we log an unexpected input one.
+        else:
+            rejected.append((key, value))
+    return updated, rejected
