7ec1fba1bb123f7b29aef942faf340e75b6d2377
Michael Barnett <ToMichaelBarnett@gmail.com>
2025-09-03 17:58:29 +1200

    Tidied region.py, added specific alpha-3 checking support before reverting to search, included 3 limit in the fuzzy search allowance, which in particular allows United States as a search term.

--- commec/regulation/region.py
+++ commec/regulation/region.py
@@ -1,8 +1,9 @@
 
 """
-Responsible for the single source of truth - either through ingestion,
-or otherwise, where store the relationship between regions that are not 
-countries themselves, but groups of countries.
+The Region module handles the sanitization, and import, of counties
+and regions. Supports all current alpha2 country codes through pycountry
+module. However also overrides this behaviour with an imported region_definitions.json
+data file, which supports general region nesting, such as EU, for the European Union.
 
 We require this distinction because of the nature of political and policy making
 entities not being a true 1:1 with countries. Furthermore, we want the user to input
@@ -70,10 +71,17 @@ def get_regions_set(region_info : str | list[str] | list[Region]) -> set[str]:
 def _return_country_set_from_unknown(region_info : str | Region = "") -> set[str]:
     """
     Given a String or Region object, return a set of alpha-2 country codes.
-    The input is checked if it is a 
-    * Region, in which case its acronym is returned in the set.
-    * Custom Region i.e. EU, returns the set() of all containing alpha-2 codes
-    * Arbitrary String, uses pycountry to fuzzy search the region, and returns the alpha-2 code.
+    The input is checked if it is a ...
+    * Region : Region, in which case its acronym is returned in the set.
+    * Custom Region : str, i.e. EU, returns the set() of all containing alpha-2 codes
+    * Arbitrary String : str, uses pycountry to fuzzy search the region, returns the alpha-2 code.
+
+    The use of an arbitrary string is typically unhelpful, unless
+    the search term was specific enough to disambigiously identify a single
+    region, or small number of regions (<=3 currently). 
+    Otherwise, an error is printed, and no country is chosen.
+    This is mainly chosen because United States as a search term
+    returns 3 [US, UM, VI] (US minor islands, and Virgin Islands)
     """
     global REGION_DATA_LUT
 
@@ -94,18 +102,39 @@ def _return_country_set_from_unknown(region_info : str | Region = "") -> set[str
         if retrieved_country:
             return set([search_string])
 
+    # Try 3 letter code retrieval
+    if len(search_string) == 3:
+        retrieved_country = pc.countries.get(alpha_3 = search_string)
+        if retrieved_country:
+            return set([retrieved_country.alpha_2])
+
     # Search with pycountry for alpha_2 codes.
     try:
         countries = pc.countries.search_fuzzy(search_string)
-    except(LookupError):
+    except LookupError:
         logger.error("Unrecognised region data: \"%s\"", search_string)
         return set()
 
-    if len(countries) > 1:
-        logger.debug("%i countries were returned for input \"%s\".",
+    # If the search returned with 3 or less outcomes its probably right.
+    # This IF statement can be deleted if this behaviour is undesired.
+    if len(countries) > 1 and len(countries) <= 3:
+        logger.warning("%i countries were returned for input \"%s\".",
                      len(countries), search_string)
+        country_list = ",".join([country.alpha_2 for country in countries])
+        logger.warning("Countries from search: %s", country_list)
+        logger.warning("Using first country: %s", countries[0].name)
+        return set([countries[0].alpha_2])
 
-    return [countries[0].alpha_2]
+    # If too many values returned, annoy the user.
+    if len(countries) > 1:
+        logger.error("%i countries were returned for input \"%s\".",
+                     len(countries), search_string)
+        country_list = ",".join([country.alpha_2 for country in countries])
+        logger.error("Countries from search: %s", country_list)
+        logger.error("Please refine your input to disambigiously identify a specific country.")
+        return set()
+
+    return set([countries[0].alpha_2])
 
 
 
