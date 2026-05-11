87f2eb15c6357e66a6d8ceac0d16a69fc5711a27
manu-script <manu.ibbis@gmail.com>
2026-05-11 11:23:23 +0200

    Changed multithreading mode of BLASTX for better scaling of large number of queries

--- commec/tools/blastx.py
+++ commec/tools/blastx.py
@@ -25,6 +25,7 @@ class BlastXHandler(BlastHandler):
         self.arguments_dictionary = {
             "-task": "blastx-fast",
             "-num_threads": self.threads,
+            "-mt_mode": 1,
             "-evalue": 1e-10,
             "-max_target_seqs": 500,
             "-culling_limit": 1,
