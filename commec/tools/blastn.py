a55c0bc1cc3f293ed82731b46f37b938ea702569
Michael Barnett <Michael@ibbis.bio>
2026-05-14 15:47:36 +1200

    Restored old database blast instructions in commandline.

--- commec/tools/blastn.py
+++ commec/tools/blastn.py
@@ -23,14 +23,6 @@ class BlastNHandler(BlastHandler):
         super().__init__(database_file, input_file, out_file, **kwargs)
         # We fill this with defaults, however they can always be overridden before screening.
         self.arguments_dictionary = {
-            "-task": "dc-megablast",
-            "-template_type": "optimal",
-            "-template_length": 16,
-            "-num_threads": self.threads,
-            "-mt_mode": 1,
-            "-evalue": 1e-5,
-            "-max_target_seqs": 500,
-            "-culling_limit": 1,
             "-outfmt": [
                 "7",
                 "qacc",
@@ -47,7 +39,38 @@ class BlastNHandler(BlastHandler):
                 "sstart",
                 "send",
             ],
+            "-num_threads": self.threads,
+            "-evalue": 10,
+            "-max_target_seqs": 50,
+            "-culling_limit": 5,
         }
+
+        #self.arguments_dictionary = {
+        #    "-task": "dc-megablast",
+        #    "-template_type": "optimal",
+        #    "-template_length": 16,
+        #    "-num_threads": self.threads,
+        #    "-mt_mode": 1,
+        #    "-evalue": 1e-5,
+        #    "-max_target_seqs": 500,
+        #    "-culling_limit": 1,
+        #    "-outfmt": [
+        #        "7",
+        #        "qacc",
+        #        "stitle",
+        #        "sacc",
+        #        "staxids",
+        #        "evalue",
+        #        "bitscore",
+        #        "pident",
+        #        "qlen",
+        #        "qstart",
+        #        "qend",
+        #        "slen",
+        #        "sstart",
+        #        "send",
+        #    ],
+        #}
         self.blastcall = "blastn"
 
     def _search(self):
