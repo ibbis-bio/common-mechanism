a562bf4c1b437468ac491a4b44d3951f8ae556e3
Michael Barnett <Michael@ibbis.bio>
2026-07-08 11:18:14 +1200

    Ensured the new database arguments are actually applied.

--- commec/tools/blastn.py
+++ commec/tools/blastn.py
@@ -25,6 +25,14 @@ class BlastNHandler(BlastHandler):
         super().__init__(database_file, input_file, out_file, **kwargs)
         # We fill this with defaults, however they can always be overridden before screening.
         self.arguments_dictionary = {
+            "-task": "dc-megablast",
+            "-template_type": "optimal",
+            "-template_length": 16,
+            "-num_threads": self.threads,
+            "-mt_mode": 1,
+            "-evalue": 1e-5,
+            "-max_target_seqs": 500,
+            "-culling_limit": 1,
             "-outfmt": [
                 "7",
                 "qacc",
@@ -41,38 +49,7 @@ class BlastNHandler(BlastHandler):
                 "sstart",
                 "send",
             ],
-            "-num_threads": self.threads,
-            "-evalue": 10,
-            "-max_target_seqs": 50,
-            "-culling_limit": 5,
         }
-
-        #self.arguments_dictionary = {
-        #    "-task": "dc-megablast",
-        #    "-template_type": "optimal",
-        #    "-template_length": 16,
-        #    "-num_threads": self.threads,
-        #    "-mt_mode": 1,
-        #    "-evalue": 1e-5,
-        #    "-max_target_seqs": 500,
-        #    "-culling_limit": 1,
-        #    "-outfmt": [
-        #        "7",
-        #        "qacc",
-        #        "stitle",
-        #        "sacc",
-        #        "staxids",
-        #        "evalue",
-        #        "bitscore",
-        #        "pident",
-        #        "qlen",
-        #        "qstart",
-        #        "qend",
-        #        "slen",
-        #        "sstart",
-        #        "send",
-        #    ],
-        #}
         self.blastcall = "blastn"
 
     def _validate_db(self):
